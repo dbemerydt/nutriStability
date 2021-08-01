daf3e47991aea683c433edc3d9939a3e945ce4d7
dbemerydt <bfemery@uvm.edu>
2021-08-01 11:50:28 -0700

    Updating README and adding year-bounds to the input arguments of nutriStability-timeSeries.py

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# nutriStability
+# Nutritional Stability Metric Computation Repository
 
+Nutritional stability (AKA nutristability) is a measurement of a the stability of a country's nutrient supply on a given year. This stability is measured by generating a bipartite network of the crops in the food system and the nutrients they contain, and performing an attack-tolerance process on that network. This approach into account the redundancy in sources of any particular nutrient.
 
-### all code relating to the nutriStability project with Charlie Nicholson
+This repository contains the code and data used to compute these values for the our study in Nature Communications, including scripts for early exploratory analysis.
 
-crop-nutrient_network_builder.py makes a network like [this example](http://www.uvm.edu/~bfemery/nigeria_bipartite-narrow.pdf), for the country speficfied by you.
+## nutriStability-timeSeries.py
+
+The central essential script for computing nutritional stability. 
+
+### Inputs
+Takes country, source (as defined in paper, P or PI), start year, end year, as command line arguments.
+
+### Outputs
+Returns a dataframe with a row for each year containing:
+- year, 
+- mean stability, 
+- upper bound stability,
+- lower bound stability,
+- crop diversity (number of crops),
+- the 5 most connected crops in the network
+- the 5 least connected nutrients in the network
--- nutriStability-timeSeries.py
+++ nutriStability-timeSeries.py
@@ -10,12 +10,14 @@ Output includes yearly:
     - Bottom 5 LEAST connected (susceptible to loss) nutrients
     - Nutristability (mean and bounds) given the hypothetical nonexistence of each crop
 
-TO RUN FROM TERMINAL: python nutriStability-timeSeries.py $country $source
+TO RUN FROM TERMINAL: python nutriStability-timeSeries.py $country $source $startYear $endYeaer
     $country is the country name 
-    $source is P (production), PI (production+imports), or PIE (production+imports-exports)
+    $source is P (production) PI (production+imports)
+    $startYear is the first year in the desired timeseries. 
+    $endYear is the final year in the desired timeseries.
 
     example run:
-        python nutriStability-timeseries.py Liberia PI
+        python nutriStability-timeseries.py Liberia PI 2015 2016
 '''
  
 # We at least need most of these packages.
@@ -34,8 +36,10 @@ from scipy import stats
 # Input arguments
 country = str(sys.argv[1])
 typ = str(sys.argv[2])
+startYr = int(sys.argv[3])
+endYr = int(sys.argv[4])
 
-years = range(1961,2016+1)
+years = range(startYr,endYr+1)
 thresh = 0.1
 
 # Source type determines which dataset we pull from
@@ -43,10 +47,8 @@ if typ == 'P':
     production = pd.read_csv('data/production.csv',encoding = "ISO-8859-1")
 elif typ == 'PI':
     production = pd.read_csv('data/prodPlusImp.csv',encoding = "ISO-8859-1")
-elif typ == 'PIE':
-    production = pd.read_csv('data/prodPlusImpMinusExp.csv',encoding = "ISO-8859-1")
 else:
-    print('Invalid type argument. Enter "P","PI", or "PIE".')
+    print('Invalid type argument -- enter "P" or "PI".')
 
 # Import the other data we need.
 foodNutrients = pd.read_csv('data/foodNutrients-frac.csv')
@@ -54,35 +56,49 @@ servingSizes = pickle.load(open('data/servingSizes.pkl','rb'))
 population = pd.read_csv('data/API_SP.POP.TOTL_DS2_en_csv_v2_63973.csv')
 
 
-def mean_confidence_interval(df, confidence=0.5):
+def mean_confidence_interval(df, confidence=0.99):
     '''
-    Computes the upper and lower confidence bounds for the curve's height at a certain point.
+    Computes the upper and lower confidence bounds for the curve's height at each point.
     '''
     n = len(list(df))
-    m, se = df.mean(axis=1), df.sem(axis=1)
-    h = stats.t.isf((1-confidence)/2,n)*se/np.sqrt(n)
-    return m, m-h, m+h
+    m, se = df.mean(axis=1), df.sem(axis=1) # get row-wise mean and standard deviation 
+    
+    l,h = stats.t.interval(confidence, n-1, loc=m, scale=se) # compute t-interval at each point
+    l = list(l) # convert all to an easy list format
+    h = list(h)
+    m = list(m)
+    
+    for i in range(len(m)): # At the top and bottom of the curve there is no variation, so the interval is undefined.
+        if np.isnan(l[i]):  # At these points, we simply force the interval to have zero width.
+            l[i] = m[i]
+        if np.isnan(h[i]):
+            h[i] = m[i]    
+    
+    return m, l, h
+
 
 def multicurve_unweighted(H,lenL,numRuns):
     '''
-    Draws the curve given the bipartite network.
+    Draws the curve (and confidence bounds) given the bipartite network.
     '''
     df = pd.DataFrame()
     for i in range(numRuns):
         G = H.copy()
         height = []
-        height.append(len(list(set([v for u,v in list(G.edges())]))))
+        height.append(len(list(set([v for u,v in list(G.edges())])))) # the initial curve height is the number nutrient nodes with at least one link
         order = list(G.nodes())[:lenL]
+        # numNutrients = len(list(G.nodes()))-lenL # infer the number of nutrients from number of crops
+        numNutrients = 17
         random.shuffle(order)
         for node in order:
             G.remove_node(node)
-            height.append(len(list(set([v for u,v in list(G.edges())]))))
-        df[i] = height
-    df['avg'],df['lower'],df['upper'] = mean_confidence_interval(df)
-    df = df.clip(0,17)
+            height.append(len(list(set([v for u,v in list(G.edges())])))) # compute height again after removing each node
+        df[i] = height # put list of heights into dataframe
+    df['avg'],df['lower'],df['upper'] = mean_confidence_interval(df) # get mean and bounds
+    df = df.clip(0,numNutrients)
     
-    #there are 17 nutrients to begin with
-    return((df['avg']/17).tolist(),(df['lower']/17).tolist(),(df['upper']/17).tolist())
+    # normalize curves and export them as lists
+    return((df['avg']/numNutrients).tolist(),(df['lower']/numNutrients).tolist(),(df['upper']/numNutrients).tolist())
 
 def countryStability(countryName):
     '''
@@ -104,34 +120,38 @@ def countryStability(countryName):
     
 
     for year in years:
+        # Get list of unique crops in dataset for the given country/year.
         countryCrops = [x for x in list(set(production.loc[(production['Area']==countryName)&(production['Y'+str(year)]>0),'Item'].tolist())) if x in list(servingSizes.keys())]
         # Initialize network from crops and nutrients
-        bnk = nx.OrderedGraph()
+        bnk = nx.OrderedGraph() 
         bnk.add_nodes_from(countryCrops, bipartite=0)
         bnk.add_nodes_from(nutrientList, bipartite=0)
         edges = []
         weights = []
         cropDegrees = {}
         
-        #add edges, determine weight, then remove edges that are too light
+        # add edges, determine weight, then remove edges that are too light
         for crop in countryCrops:
             newEdges = []
             for nutrient in nutrientList:
-                weight = np.mean(foodNutrients.loc[foodNutrients['FAO_name']==crop,nutrient])
-                weight = weight * 10**6 * 10**(-2) * (1/servingSizes[crop]) / population[population['Country Name']==countryName][str(year)].iloc[0]
-                weight = weight * np.mean(production.loc[(production['Area']==countryName)&(production['Item']==crop)]['Y'+str(year)])
-                if weight>0.1:
+                weight = np.mean(foodNutrients.loc[foodNutrients['FAO_name']==crop,nutrient]) # Weight begins as proportional to amount of nutrient
+                weight = weight * 10**6 * 10**(-2) * (1/servingSizes[crop]) / population[population['Country Name']==countryName][str(year)].iloc[0] # normalize by serving size and population 
+                weight = weight * np.mean(production.loc[(production['Area']==countryName)&(production['Item']==crop)]['Y'+str(year)]) # include a correction factor for the actual amount of the crop
+                if weight>0.1: # only add the edge if it's heavy enough
                     newEdges.append((crop, nutrient, weight))
-                    weights.append(weight)
-            edges.extend(newEdges)
-            bnk.add_weighted_edges_from(newEdges)
+                    weights.append(weight) 
+            edges.extend(newEdges) 
+            bnk.add_weighted_edges_from(newEdges) # put the edges on the network
             cropDegrees[crop] = bnk.degree[crop]
             if bnk.degree[crop] == 0:
-                bnk.remove_node(crop)
-                countryCrops = [x for x in countryCrops if x!=crop]
+                bnk.remove_node(crop) # remove the crop if it had no edges connecting to it
+                countryCrops = [x for x in countryCrops if x!=crop] 
         nutrDegrees = {}
-        for nutrient in nutrientList:
+        for nutrient in nutrientList: # record the degree of each nutrient
             nutrDegrees[nutrient] = bnk.degree[nutrient]
+
+        adjacency_frame = nx.to_pandas_adjacency(bnk) #output the crop-nutrient adjacency matrix
+        adjacency_frame.to_csv('data/'+str(country)+'-'+str(typ)+'-'+str(year)+'-crop_nutrient_dataframe.csv')
 
 
         # record network metrics
@@ -143,7 +163,7 @@ def countryStability(countryName):
 
 
         # draw curves and take integrals
-        curve,lower,upper = multicurve_unweighted(bnk,len(countryCrops),100)    
+        curve,lower,upper = multicurve_unweighted(bnk,len(countryCrops),1000)    
         stabilities_m.append(sum([curve[i]/len(curve) for i in range(len(curve))]))
         stabilities_u.append(sum([upper[i]/len(upper) for i in range(len(upper))]))
         stabilities_l.append(sum([lower[i]/len(lower) for i in range(len(lower))]))
@@ -154,7 +174,7 @@ def countryStability(countryName):
         for crop in countryCrops:
             bnkt = bnk.copy()
             bnkt.remove_node(crop)
-            curve,lower,upper = multicurve_unweighted(bnkt,len(countryCrops),100)
+            curve,lower,upper = multicurve_unweighted(bnkt,len(countryCrops),1000)
             s_m=(sum([curve[i]/len(curve) for i in range(len(curve))]))
             s_u=(sum([upper[i]/len(upper) for i in range(len(upper))]))
             s_l=(sum([lower[i]/len(lower) for i in range(len(lower))]))
@@ -169,6 +189,7 @@ def countryStability(countryName):
 # put all the data into an organized frame  and save.
 stabilities_m,stabilities_u,stabilities_l,numCrops,topCrops,botNutrs,avgDegCrops,avgDegNutrs,removed_crop_stabilities_list = countryStability(country)
 df = pd.DataFrame()
+df['year'] = years
 df[country+'_m'] = stabilities_m
 df[country+'_l'] = stabilities_l
 df[country+'_u'] = stabilities_u
@@ -178,7 +199,7 @@ df[country+'_botNutrs'] = botNutrs
 df[country+'_avgDegCrops'] = avgDegCrops
 df[country+'_avgDegNutrs'] = avgDegNutrs
 df[country+'_removed_crop_stabilities'] = removed_crop_stabilities_list
-df.to_csv('data/timeseries/nutriStability-timeseries-'+country+'-'+typ+'.csv')
+df.to_csv('data/17/nutriStability-timeseries-'+country+'-'+typ+'.csv',index=False)
 
 
 
