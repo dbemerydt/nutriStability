64fa92ffaa70f4d53a3b685361ef7a3d1e2f24a5
dbemerydt <bfemery@uvm.edu>
2021-08-01 13:10:47 -0700

    annotating curvorator scripts for clarity and including them as well the abstract in the readme.

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Nutritional stability (AKA nutristability) is a measurement of a the stability of a country's nutrient supply on a given year. This stability is measured by generating a bipartite network of the crops in the food system and the nutrients they contain, and performing an attack-tolerance process on that network. This approach into account the redundancy in sources of any particular nutrient.
 
-This repository contains the code and data used to compute these values for the our study in Nature Communications, including scripts for early exploratory analysis.
+This repository contains the code and data used to compute these values for the our study in Nature Communications (DOI pending), including scripts for early exploratory analysis.
+
+## Abstract:
+
+Nutritional stability – a food system’s capacity to provide sufficient nutrients despite disturbance – is an important, yet challenging to measure outcome of diversified agriculture. Using 55 years of data across 184 countries, we assemble 22,000 bipartite crop-nutrient networks to quantify nutritional stability by simulating crop and nutrient loss in a country, and assess its relationship to crop diversity across regions, over time and between imports versus in country production. We find a positive, saturating relationship between crop diversity and nutritional stability across countries, but also show that over time nutritional stability remained stagnant or decreased in all regions except Asia. These results are attributable to diminishing returns on crop diversity, with recent gains in crop diversity among crops with fewer nutrients, or with nutrients already in a country’s food system. Finally, imports are positively associated with crop diversity and nutritional stability, indicating that many countries’ nutritional stability is market exposed.
 
 ## nutriStability-timeSeries.py
 
@@ -20,3 +24,28 @@ Returns a dataframe with a row for each year containing:
 - crop diversity (number of crops),
 - the 5 most connected crops in the network
 - the 5 least connected nutrients in the network
+
+
+
+## nutriStabilityCurvorator.py
+
+A script for plotting the nutriStability curve for a particular country/year.
+
+### Inputs
+Takes country, year, and source as command line arguments.
+
+### Outputs
+Returns a plot as a pdf file with the curve and the nutristability value.
+
+
+## nutriStabilityCurvorator-networkBuilder.py 
+
+For plotting the nutriStability curve and the crop-nutrient bipartite network for a country/year.
+
+### Inputs
+Takes country, year, and source as command line arguments.
+
+### Outputs
+Returns a plot as a pdf file with the curve and the nutristability value, and a plot of the crop-nutrient bipartite network.
+
+
--- nutriStabilityCurvorator.py
+++ nutriStabilityCurvorator.py
@@ -1,3 +1,19 @@
+'''
+nutriStabilityCurvorator -- Ben Emery 2019
+
+Compute nutriStability and produce curve visual given a country, year, 
+link threshold, source (P or PI), plots the average curve.
+
+TO RUN FROM TERMINAL: python nutriStabilityCurvorator.py $country $year $source
+    $country is the country name 
+    $year is year
+    $source is P (production) PI (production+imports)
+
+    example run:
+        python nutriStabilityCurvorator.py Liberia 2015 PI
+'''
+
+
 import pandas as pd
 import glob
 import csv
@@ -13,6 +29,9 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 def multicurve_unweighted(H,lenL,numRuns):
+    '''
+    Draws the curve (and confidence bounds) given the bipartite network.
+    '''
     df = pd.DataFrame()
     for i in range(numRuns):
         G = H.copy()
@@ -23,49 +42,61 @@ def multicurve_unweighted(H,lenL,numRuns):
         for node in order:
             G.remove_node(node)
             height.append(len(list(set([v for u,v in list(G.edges())]))))
-        df[i] = height
+        df[i] = height # put list of heights into dataframe
     df['avg'] = df.mean(axis=1)
     
     #there are 17 nutrients to begin with
-    return((df['avg']/17).tolist())
+    return((df['avg']/17).tolist()) # normalize curves and export them as lists
 
 
 countryName = sys.argv[1]
 year = sys.argv[2]
-thresh = sys.argv[3]
+typ = sys.argv[3]
 
-production = pd.read_csv('data/Production_Crops_E_All_Data.csv',encoding = "ISO-8859-1")
+# Source type determines which dataset we pull from
+if typ == 'P':
+    production = pd.read_csv('data/production.csv',encoding = "ISO-8859-1")
+elif typ == 'PI':
+    production = pd.read_csv('data/prodPlusImp.csv',encoding = "ISO-8859-1")
+else:
+    print('Invalid type argument -- enter "P" or "PI".')
+
+# Import the other data we need.
 foodNutrients = pd.read_csv('data/foodNutrients-frac.csv')
 servingSizes = pickle.load(open('data/servingSizes.pkl','rb'))
 population = pd.read_csv('data/API_SP.POP.TOTL_DS2_en_csv_v2_63973.csv')
 
-
+# Get lists of unique crops and nutrients in dataset for the given country/year.
 countryCrops = [x for x in list(set(production.loc[(production['Area']==countryName)&(production['Y'+str(year)]>0),'Item'].to_list())) if x in list(servingSizes.keys())]
-nutrientList = list(foodNutrients)[4:-1]
+nutrientList = [x for x in list(foodNutrients)[4:-1] if x!='Sodium']
 
+# Initialize network from crops and nutrients
 bnk = nx.OrderedGraph()
 bnk.add_nodes_from(countryCrops, bipartite=0)
 bnk.add_nodes_from(nutrientList, bipartite=0)
 edges = []
 weights = []
+# add edges, determine weight, then remove edges that are too light
 for crop in countryCrops:
     newEdges = []
     for nutrient in nutrientList:
         weight = np.mean(foodNutrients.loc[foodNutrients['FAO_name']==crop,nutrient])
         weight = weight * 10**6 * 10**(-2) * (1/servingSizes[crop]) / population[population['Country Name']==countryName][year].iloc[0]
         weight = weight * np.mean(production.loc[(production['Area']==countryName)&(production['Item']==crop)]['Y'+year])
-        if weight>0.1:
+        if weight>0.1: # only add the edge if it's heavy enough
             newEdges.append((crop, nutrient, weight))
             weights.append(weight)
         edges.extend(newEdges)
-        bnk.add_weighted_edges_from(newEdges)
+        bnk.add_weighted_edges_from(newEdges) # put the edges on the network
     if bnk.degree[crop] == 0:
-        bnk.remove_node(crop)
+        bnk.remove_node(crop) # remove the crop if it had no edges connecting to it
         countryCrops = [x for x in countryCrops if x!=crop]
 
 
+# draw curve and take integral
 curve = multicurve_unweighted(bnk,len(countryCrops),1000)
 
+# plot curve with nutriStability value included.
 plt.plot([i/(len(curve)-1) for i in range(len(curve))],curve,color='dodgerblue')
 
 ax = plt.gca()
